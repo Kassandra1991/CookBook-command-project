e2a6a7cf4b03900842cafd5522b18c96925c303b
Alex <alex@MacBook-Pro-Alex.local>
2023-02-26 22:47:47 +0300

    Complete prepare

---
+++ СookBook/Application/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  СookBook
+//
+//  Created by Alex on 26.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
---
+++ СookBook/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  СookBook
+//
+//  Created by Alex on 26.02.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+}
+
