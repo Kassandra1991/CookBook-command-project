2c81301581f179e1caac799d8f9e00df3bd70e50
AndreiKonan <konon.andrei84@gmail.com>
2023-02-28 01:54:48 +0300

    ADD: created 2 controllers: FavoriteViewController and SearchViewController 	 created TabBar

--- СookBook/Application/SceneDelegate.swift
+++ СookBook/Application/SceneDelegate.swift
@@ -14,11 +14,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
         window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = createTabBarController()
+        
+        
+        func createTabBarController() -> UITabBarController {
+            let tabBarController = UITabBarController()
+            UITabBar.appearance().backgroundColor = .systemBackground
+            tabBarController.viewControllers = [createFavoriteViewController(), createSearchViewController()]
+            return tabBarController
+        }
+        
+        func createFavoriteViewController() -> UINavigationController {
+            let favoriteViewController = FavoriteViewController()
+            favoriteViewController.title = "Favorite"
+            favoriteViewController.tabBarItem = UITabBarItem(title: "Favorite", image: UIImage(systemName: "Star"), tag: 0)
+            return UINavigationController(rootViewController: favoriteViewController)
+        }
+        
+        func createSearchViewController() -> UINavigationController {
+            let searchViewController = SearchViewController()
+            searchViewController.title = "Search"
+            searchViewController.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "Search"), tag: 1)
+            return UINavigationController(rootViewController: searchViewController)
+        }
     }
 }
 
---
+++ СookBook/ViewController/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  СookBook
+//
+//  Created by Admin on 28.02.23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+        
+    }
+
+}
