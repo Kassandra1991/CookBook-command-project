590014f0ad12dd3ca37137b80aa958931252e3fc
Alex Asichka <sanechka3@mail.ru>
2023-02-27 14:48:23 +0200

    first settings for new file

---
+++ СookBook/ViewController/MainViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MainViewController.swift
+//  СookBook
+//
+//  Created by Aleksandra Asichka on 2023-02-27.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - constants
+    
+    // MARK: - property
+    
+    // MARK: - life cycle funcs
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubViews()
+        configure()
+        setConstraints()
+
+    }
+    
+// MARK: - flow funcs
+    private func configure() {
+        configureView()
+        configureLabel()
+        configureButton()
+    }
+    
+    private func addSubViews() {
+        
+    }
+    
+    private func configureView() {
+        
+    }
+    
+    private func configureLabel() {
+        
+    }
+    
+    private func configureButton() {
+        
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+        ])
+    }
+}
+
+// MARK: - Delegate
