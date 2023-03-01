3c632653ffb04ae66254668d0c573965efc39f90
Dmitriy Babichev <lightonlyshines@gmail.com>
2023-03-01 23:12:23 +0300

    favorites vc, some minor fixes

---
+++ СookBook/ViewController/FavoritesViewController.swift
@@ -0,0 +1,121 @@
+//
+//  FavoritesViewController.swift
+//  СookBook
+//
+//  Created by Dmitriy Babichev on 28.02.2023.
+//
+
+import UIKit
+
+final class FavoritesViewController: UIViewController {
+
+    // MARK: - properties
+    private let titleLabel = UILabel()
+    private let tableView = UITableView()
+    private var cellObjects = [FavoriteRecipe]()
+
+    // MARK: - life cycle funcs
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubViews()
+        configure()
+        setConstraints()
+    }
+
+    // MARK: - flow funcs
+    private func configure() {
+        configureViews()
+        configureLabels()
+        configureTableView()
+        configureCellObjects()
+    }
+
+    private func addSubViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(tableView)
+    }
+
+    private func configureViews() {
+        view.backgroundColor = .systemGroupedBackground
+    }
+
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(FavoritesTableViewCell.self, forCellReuseIdentifier: FavoritesTableViewCell.identifier)
+        tableView.backgroundColor = .systemGroupedBackground
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+    }
+
+    private func configureLabels() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Favorite recipes"
+        titleLabel.textAlignment = .natural
+        titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        titleLabel.textColor = .black
+    }
+
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+
+            tableView.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 2),
+            tableView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    private func configureCellObjects() {
+        let testObjects = [FavoriteRecipe(title: "How to make french toast", imageName: "recipe-1"),
+                           FavoriteRecipe(title: "How to make sushi at home", imageName: "recipe-2"),
+                           FavoriteRecipe(title: "Easy oatmeal recipe", imageName: "recipe-3")]
+        cellObjects.append(contentsOf: testObjects)
+    }
+}
+
+    // MARK: - UITableViewDataSource
+extension FavoritesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cellObjects.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoritesTableViewCell.identifier, for: indexPath) as? FavoritesTableViewCell else {
+            return UITableViewCell()
+        }
+
+        let data = cellObjects[indexPath.row]
+        cell.configure(title: data.title, imageName: data.imageName)
+
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension FavoritesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        250
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Cell at \(indexPath.row) row tapped!")
+    }
+
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: nil) { (_, _, completionHandler) in
+            self.cellObjects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            completionHandler(true)
+        }
+        deleteAction.image = UIImage(systemName: "trash")?.withTintColor(.systemPink, renderingMode: .alwaysOriginal)
+        deleteAction.backgroundColor = .systemGroupedBackground
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
+    }
+}
+
