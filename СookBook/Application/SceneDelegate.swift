89187bfe6014c38a2119e0a3e966ce9a67564859
Alex <alex@MacBook-Pro-Alex.local>
2023-03-07 23:37:53 +0300

    modified SceneDelegate

--- СookBook/Application/SceneDelegate.swift
+++ СookBook/Application/SceneDelegate.swift
@@ -16,15 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
-        let startVC = StartViewController()
-        let navigation = UINavigationController(rootViewController: startVC)
-        window?.rootViewController = navigation
-        window?.makeKeyAndVisible()
         window?.rootViewController = StartViewController()
-    }
-
-    func configureHomeController() -> UIViewController {
-        let recipeNavVC = UINavigationController(rootViewController: RecipeViewController())
-        return recipeNavVC
+        window?.makeKeyAndVisible()
     }
 }
