d5fcc9d854581421dcdcf3a179590de2683d1972
Dmitriy Babichev <lightonlyshines@gmail.com>
2023-03-10 21:01:00 +0300

    Core Data implementation, saving recipes feature

---
+++ СookBook/Models/Core Data/Recipe+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe+CoreDataClass.swift
+//  СookBook
+//
+//  Created by Dmitriy Babichev on 09.03.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Recipe)
+public class Recipe: NSManagedObject {
+
+}
